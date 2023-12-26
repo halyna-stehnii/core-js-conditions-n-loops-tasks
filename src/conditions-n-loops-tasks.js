959c6aa645dc7c84fa1ab6979f9cdd1e7cbb27cd
Halyna Stehnii <galyna.stegniy@gmail.com>
2023-12-26 23:44:59 +0100

    feat: isIsoscelesTriangle solved

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -84,8 +84,13 @@ function canQueenCaptureKing(/* queen, king */) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  if (a > 0 && b > 0 && c > 0) {
+  if (a === b && a + b > c) return true;
+  if (a === c && a + c > b) return true;
+  if (b === c && b + c > a) return true;
+  }
+  return false;
 }
 
 /**
