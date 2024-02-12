7934cb4953ff5b6d617d8c62b4598e5d028a476f
Halyna Stehnii <galyna.stegniy@gmail.com>
2024-02-12 22:25:01 +0100

    feat: isContainNumber

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -266,8 +266,14 @@ function getIndexOf(str, letter) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  const numStr = String(num);
+  for (let i = 0; i < numStr.length; i += 1) {
+    if (parseInt(numStr[i], 10) === digit) {
+      return true;
+    }
+  }
+  return false;
 }
 
 /**
