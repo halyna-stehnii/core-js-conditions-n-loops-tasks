ee4557ee18e9de2bc4dbb2d6d4b055e58b8470fc
Halyna Stehnii <galyna.stegniy@gmail.com>
2024-02-12 21:51:32 +0100

    feat: convertNumberToString

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -174,8 +174,37 @@ function convertToRomanNumerals(num) {
  *  '10,5'    => 'one zero point five'
  *  '1950.2'  => 'one nine five zero point two'
  */
-function convertNumberToString(/* numberStr */) {
-  throw new Error('Not implemented');
+function convertNumberToString(numberStr) {
+  const words = {
+    0: 'zero',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    '-': 'minus',
+    '.': 'point',
+    ',': 'point',
+  };
+  let result = '';
+
+  for (let i = 0; i < numberStr.length; i += 1) {
+    const char = numberStr[i];
+
+    switch (i) {
+      case numberStr.length - 1:
+        result += `${words[char]}`;
+        break;
+      default:
+        result += `${words[char]} `;
+        break;
+    }
+  }
+  return result;
 }
 
 /**
